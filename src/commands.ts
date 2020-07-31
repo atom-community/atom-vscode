588ebce364740cbbaea7384a0e01ddf8ee64340c
aminya <aminyahyaabadi74@gmail.com>
2020-07-30 19:08:03 -0500

    feat: executeCommand

--- src/commands.ts
+++ src/commands.ts
@@ -50,3 +50,38 @@ export function registerTextEditorCommand(
   return atom.commands.add(target, command, callback);
 }
 
+/**
+ * Executes the command denoted by the given command identifier.
+ *
+ * * *Note 1:* When executing an editor command not all types are allowed to
+ * be passed as arguments. Allowed are the primitive types `string`, `boolean`,
+ * `number`, `undefined`, and `null`, as well as [`Position`](#Position), [`Range`](#Range), [`Uri`](#Uri) and [`Location`](#Location).
+ * * *Note 2:* There are no restrictions when executing commands that have been contributed
+ * by extensions.
+ *
+ * @param command Identifier of the command to execute.
+ * @param rest Parameters passed to the command function.
+ * @return A thenable that resolves to the returned value of the given command. `undefined` when
+ * the command handler function doesn't return anything.
+ */
+export function executeCommand<T>(
+  command: string,
+  ...rest: any[]
+): Thenable<T | undefined> {
+  let target;
+  const prefix = command.split(".")[0];
+  if (prefix === "vscode") {
+    target = atom.views.getView(atom.workspace);
+  } else if (prefix === "editor") {
+    const editor = atom.workspace.getActiveTextEditor();
+    if (editor) {
+      target = atom.views.getView(editor);
+    }
+  }
+  // else search for the command
+  if (rest) {
+    console.warn("rest parameters are not supported.");
+  }
+  return atom.commands.dispatch(target, command);
+}
+
