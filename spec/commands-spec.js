071bb510a46553ef57093078b0df7b76cdda1810
aminya <aminyahyaabadi74@gmail.com>
2020-08-02 11:33:33 -0500

    test: commands spec

---
+++ spec/commands-spec.js
@@ -0,0 +1,25 @@
+"use babel";
+
+import { commands } from "../dist/main.js";
+
+describe("Commands", () => {
+  it("registers a command for the workspace and executes it", () => {
+    commands.registerCommand("package1.command1", () => console.log("command1"));
+    expect(atom.commands.registeredCommands["package1.command1"]).toBe(true);
+
+    spyOn(console, "log")
+    commands.executeCommand("package1.command1")
+    expect(console.log).toHaveBeenCalled();
+
+  });
+
+  it("registers a command for the text editor and executes it", () => {
+    commands.registerTextEditorCommand("package1.command2", () => console.warn("command2"));
+    expect(atom.commands.registeredCommands["package1.command2"]).toBe(true);
+
+    spyOn(console, "warn")
+    commands.executeCommand("package1.command2")
+    expect(console.warn).toHaveBeenCalled();
+  });
+
+});
